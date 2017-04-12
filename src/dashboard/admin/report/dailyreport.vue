de1d7a72144ec3fe7ff20fa8865842929d713d40
ericwang1902 <2248930513@qq.com>
2017-04-12 19:17:19 +0800

    日报表页面。

--- src/dashboard/admin/report/dailyreport.vue
+++ src/dashboard/admin/report/dailyreport.vue
@@ -1,11 +1,10 @@
 <template>
     <div>
         <el-card class="box-card" style="margin: 10px">
-            <el-table border :data="supplierlistData" style="width: 100%">
-                <el-table-column label="供应商编号" prop="suppliernum"></el-table-column>
-                <el-table-column label="供应商名称" prop="suppliername"></el-table-column>
-                <el-table-column label="供应商说明" prop="supplierdes"></el-table-column>
-                <el-table-column label="今日营业额" prop="supplieruser.district.province"></el-table-column>
+            <el-table border :data="sda" style="width: 100%">
+                <el-table-column label="供应商名称" prop="supplierdoc.suppliername"></el-table-column>
+                <el-table-column label="供应商说明" prop="supplierdoc.supplierdes"></el-table-column>
+                <el-table-column label="今日营业额" prop="orderamount"></el-table-column>
             </el-table>
         </el-card>
     </div>
