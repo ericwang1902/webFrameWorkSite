cbc9ead07fb8d17233372cce98ac95a196b7359b
ericwang1902 <2248930513@qq.com>
2017-04-27 17:58:58 +0800

    427

--- src/dashboard/admin/supplier/supplierwechat.vue
+++ src/dashboard/admin/supplier/supplierwechat.vue
@@ -8,8 +8,7 @@
         props: ["supRow"],
         created () {
             if(supRow.supplieruser.openid){
-                
-
+               
                 //去调取后台接口来获取微信用户信息
                 //this.axios.get()
 
